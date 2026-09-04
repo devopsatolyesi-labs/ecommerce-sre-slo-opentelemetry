7931583455175950a74d0068072265d43dc0b4cf
DevOps Atölyesi Platform Team <info@devopsatolyesi.com>
2026-09-04 17:36:06 +0300

    fix(eks): remove redundant access_config to avoid forced replacement of active cluster

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -27,10 +27,6 @@ resource "aws_eks_cluster" "this" {
     endpoint_public_access  = true
   }
 
-  access_config {
-    authentication_mode                         = "API_AND_CONFIG_MAP"
-    bootstrap_cluster_creator_admin_permissions = true
-  }
 
   tags = {
     Environment = var.environment
